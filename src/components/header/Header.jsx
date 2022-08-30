479152f1fbb1761ac32a0e5feafb4d8d58c98a34
JonsMota <jonsmota@gmail.com>
2022-08-30 13:16:35 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,16 @@ import styles from './Header.module.css'
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
 
+import Icon from '../icon/Icon'
+
 function Header () {
   return(
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
-      <Subtitle>Criado por Jonas</Subtitle>  
+      <Subtitle>Criado por Jonas</Subtitle> 
+      <div className={styles.iconContent}>
+        <Icon iconName="github" Link="https://github.com/JonsMota" />
+      </div> 
     </div> 
   )  
 }
